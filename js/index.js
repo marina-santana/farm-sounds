c98c67ea3fab673ac5a08e54d78b611bb4d94e12
Marina Santana <marinasantana7894@gmail.com>
2020-09-04 21:41:57 -0300

    Easter egg adicionado

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$('.tooltip').on('mouseover', function () {
+    var mouse = event.pageX;
+    $(this).append("<style>.tooltip::after { left: " + mouse + "px; }</style>");
+});
+
+$('.tooltip').on('mouseout', function () {
+    $('.tooltip style').remove();
+});
+
+
+$('button, #img-barn').on('click', function () {
+    let next = $(this).next().attr('id')
+
+    sound = document.getElementById(next);
+
+    sound.play();
+});
+
+Mousetrap.bind('a', function () { document.getElementById('a').play(); });
+Mousetrap.bind('w', function () { document.getElementById('w').play(); });
+Mousetrap.bind('y', function () { document.getElementById('y').play(); });
+Mousetrap.bind('v', function () { document.getElementById('v').play(); });
+Mousetrap.bind('f', function () { document.getElementById('f').play(); });
+Mousetrap.bind('k', function () { document.getElementById('k').play(); });
